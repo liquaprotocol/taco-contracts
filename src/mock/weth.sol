a3921c02be3e66f6733dcbad2efa0d7c53ae975e
eth2the-moon <278338249@qq.com>
2024-03-05 12:20:47 +0800

    Change the solc verison

--- src/mock/weth.sol
+++ src/mock/weth.sol
@@ -14,7 +14,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
